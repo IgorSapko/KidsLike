cff463522f74c649fbdd3787374ff5fc1156aab0
Elina <elisonfog@gmail.com>
2021-03-01 23:44:49 +0200

    connected CurrentDay component with store

--- src/components/Commons/CurrentDay/CurrentDay.js
+++ src/components/Commons/CurrentDay/CurrentDay.js
@@ -1,18 +1,24 @@
 import React from 'react';
-// import ProgressBar from '@ramonak/react-progress-bar';
+import ProgressBar from '@ramonak/react-progress-bar';
 import { LeftSide, ProgressDiv, Container, Points, WeekText } from './currentDay.styles';
 import CardList from '../CardList/CardList';
+import { useSelector } from 'react-redux';
 
-function CurrentDay({ dayStart, dayEnd, month, dayCurrent, weekPoints, plan }) {
+function CurrentDay() {
+	const weekPoints = useSelector(state => state.week.pointsGained);
+	const plan = useSelector(state => state.week.pointsPlanned);
+	const dayStart = useSelector(state => state.week.startWeekDate);
+	const dayEnd = useSelector(state => state.week.endWeekDate);
+
 	const progress = (parseInt(weekPoints) / parseInt(plan)) * 100;
 	return (
 		<div>
 			<Container>
 				<LeftSide>
 					<WeekText>
-						Неделя {dayStart} - {dayEnd} {month}
+						Неделя {dayStart} - {dayEnd} месяц
 					</WeekText>
-					<Points>Мoи задачи: {dayCurrent}</Points>
+					<Points>Мoи задачи: день</Points>
 				</LeftSide>
 
 				<div>
@@ -22,14 +28,16 @@ function CurrentDay({ dayStart, dayEnd, month, dayCurrent, weekPoints, plan }) {
 						{weekPoints}/{plan}
 					</p>
 					<ProgressDiv>
-						{/* <ProgressBar
-						completed={progress}
-						bgcolor="#9ECB44"
-						width="280px"
-						height="6px"
-						borderRadius="10px"
-						labelSize="0"
-					/> */}
+						{
+							<ProgressBar
+								completed={progress}
+								bgcolor="#9ECB44"
+								width="280px"
+								height="6px"
+								borderRadius="10px"
+								labelSize="0"
+							/>
+						}
 					</ProgressDiv>
 				</div>
 			</Container>
