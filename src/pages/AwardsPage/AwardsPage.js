44bd7dcb740401cabd7d66581033d715ef57d40f
Vitaliy Derda <derdavitaliy@gmail.com>
2021-03-17 01:01:38 +0200

    fix/updated gift-page and removed giftOrderAction from loader reducer

--- src/pages/AwardsPage/AwardsPage.js
+++ src/pages/AwardsPage/AwardsPage.js
@@ -1,50 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import Card from '../../components/Commons/Card/Card';
-// import AwardsSubmitButton from '../../components/Commons/AwardsSubmitButton/AwardsSubmitButton';
-// import CongratsModal from '../../components/Commons/CongratsModal/CongratsModal';
-// import { BlockPeopleTask_Awards } from './AwardsPage.styles';
-// import selector from '../../redux/selectors';
-// import weekOperation from '../../redux/week/weekOperation';
-
-// export default function AwardsPage() {
-// 	const gifts = useSelector(selector.getGifts);
-// 	const dispatch = useDispatch();
-// 	const [idItems, setidItems] = useState([]);
-// 	const [itemsAll, setItemsAll] = useState([]);
-// 	const [open, setOpen] = useState(false);
-// 	useEffect(() => {
-// 		!gifts && dispatch(weekOperation.giftsGetting());
-// 	}, [gifts]);
-
-// 	const summNumber = (id, checked) => {
-// 		if (!checked) {
-// 			setidItems(prev => [...prev, id._id]);
-// 			setItemsAll(prev => [...prev, id]);
-// 		}
-// 		if (checked) {
-// 			setidItems(idItems.filter(idItem => idItem !== id._id));
-// 			setItemsAll(itemsAll.filter(idItem => idItem._id !== id._id));
-// 		}
-// 	};
-// 	console.log('open', open);
-// 	const handleSwich = async () => {
-// 		setOpen(open => !open);
-// 	};
-
-// 	return (
-// 		<div>
-// 			<BlockPeopleTask_Awards>
-// 				{gifts &&
-// 					gifts.map(item => {
-// 						return <Card key={item._id} summNumber={summNumber} item={item} />;
-// 					})}
-// 			</BlockPeopleTask_Awards>
-// 			<AwardsSubmitButton itemsAll={itemsAll} handleSwich={handleSwich} />
-// 			{open && <CongratsModal itemsAll={itemsAll} idItems={idItems} setOpen={setOpen} />}
-// 		</div>
-// 	);
-// }
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Card from '../../components/Commons/Card/Card';
@@ -56,33 +9,59 @@ import {
 	MyPrises,
 	PriseImg,
 	PriseText,
-	AwardsPageWrapper
+	AwardsPageWrapper,
 } from './AwardsPage.styles';
 import selector from '../../redux/selectors';
 import weekOperation from '../../redux/week/weekOperation';
 import CurrentDay from 'components/Commons/CurrentDay/CurrentDay';
+import notification from 'services/notification';
+import { NotificationContainer } from 'react-notifications';
 
 export default function AwardsPage() {
-	const gifts = useSelector(selector.getGifts);
 	const dispatch = useDispatch();
+	const gifts = useSelector(selector.getGifts);
+	const balance = useSelector(state => state.auth.user.balance);
+
+	const [open, setOpen] = useState(false);
 	const [idItems, setidItems] = useState([]);
 	const [itemsAll, setItemsAll] = useState([]);
-	const [open, setOpen] = useState(false);
-	useEffect(() => {
-		!gifts && dispatch(weekOperation.giftsGetting());
-	}, [gifts]);
+
+	useEffect(() => !gifts && dispatch(weekOperation.giftsGetting()), [dispatch, gifts]);
 
 	const summNumber = (id, checked) => {
 		if (!checked) {
 			setidItems(prev => [...prev, id._id]);
 			setItemsAll(prev => [...prev, id]);
 		}
+
 		if (checked) {
 			setidItems(idItems.filter(idItem => idItem !== id._id));
 			setItemsAll(itemsAll.filter(idItem => idItem._id !== id._id));
 		}
 	};
-	const handleSwich = async () => {
+
+	const handleSwitch = () => {
+		const totalPriceOfAllGifts = itemsAll.reduce((acc, { price }) => acc + price, 0);
+
+		if (balance < totalPriceOfAllGifts) {
+			notification({
+				type: 'warning',
+				message: 'Не достаточно баллов для покупки выбранных подарков',
+			});
+
+			return;
+		}
+
+		if (itemsAll.length < 1) {
+			notification({
+				type: 'warning',
+				message: 'Ничего не выбрано',
+			});
+
+			return;
+		}
+
+		dispatch(weekOperation.giftsOrder({ giftIDs: idItems }));
 		setOpen(open => !open);
 	};
 
@@ -96,16 +75,19 @@ export default function AwardsPage() {
 					<PriseImg src="https://i.ibb.co/6YGGfkb/gift-box-1.jpg" />
 					<PriseText>МОИ ПРИЗЫ</PriseText>
 				</MyPrises>
+
 				<CurrentDay awardsPage={awardsPage} menuHeight={menuHeight} />
 			</TopWrapper>
+
 			<BlockPeopleTaskAwards>
-				{gifts &&
-					gifts.map(item => {
-						return <Card key={item._id} summNumber={summNumber} item={item} />;
-					})}
+				{gifts && gifts.map(item => <Card key={item._id} summNumber={summNumber} item={item} />)}
 			</BlockPeopleTaskAwards>
-			<AwardsSubmitButton itemsAll={itemsAll} handleSwich={handleSwich} />
-			{open && <CongratsModal itemsAll={itemsAll} idItems={idItems} setOpen={setOpen} />}
+
+			<NotificationContainer />
+
+			<AwardsSubmitButton itemsAll={itemsAll} handleSwitch={handleSwitch} />
+
+			{open && <CongratsModal itemsAll={itemsAll} setOpen={setOpen} />}
 		</AwardsPageWrapper>
 	);
 }
