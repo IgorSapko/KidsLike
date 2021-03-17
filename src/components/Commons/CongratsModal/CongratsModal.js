a67bae1e413868e71bc9bc5f21d7437b11de1235
Vitaliy Derda <derdavitaliy@gmail.com>
2021-03-18 01:30:49 +0200

    fix/updated gift modal: added _close modal by backdrop and esc_

--- src/components/Commons/CongratsModal/CongratsModal.js
+++ src/components/Commons/CongratsModal/CongratsModal.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
-	ModalWrapper,
 	ImgCat,
 	Congrats,
 	BtnClose,
@@ -13,30 +12,42 @@ import {
 import ModalBackDrop from '../ModalBackDrop/ModalBackDrop';
 
 const CongratsModal = ({ itemsAll, setOpen }) => {
-	const [showModal, setShowModal] = useState(true);
+	const handleCloseModal = useCallback(
+		({ code, target }) => {
+			if (code === 'Escape' || target.id === 'backdrop') {
+				setOpen(false);
+			}
+		},
+		[setOpen],
+	);
 
-	const closeModal = () => {
-		setOpen(false);
-		setShowModal(showModal => !showModal);
-	};
+	useEffect(() => {
+		window.addEventListener('click', handleCloseModal);
+		window.addEventListener('keydown', handleCloseModal);
+
+		return () => {
+			window.removeEventListener('click', handleCloseModal);
+			window.removeEventListener('keydown', handleCloseModal);
+		};
+	}, [handleCloseModal]);
 
 	return (
 		<ModalBackDrop>
-			<ModalWrapper onClick={closeModal}>
-				<ModalCongrats showModal={showModal}>
-					<ImgCat src="https://storage.googleapis.com/kidslikev2_bucket/4c67b0f578c6e7ff1d0b504f8019ffc9.png" />
-					<Congrats>Поздравляем! Ты получаешь:</Congrats>
-					<BtnClose src="https://storage.googleapis.com/kidslikev2_bucket/08942ec472518083fbe6e2a6fb95674d.jpg" />
-					<PrizesWrapper>
-						{itemsAll.map(({ _id, title, imageUrl }) => (
-							<PrizesWrapper_block key={_id}>
-								<ChosenPrise src={imageUrl} />
-								<ChosenPrise_name>{title}</ChosenPrise_name>
-							</PrizesWrapper_block>
-						))}
-					</PrizesWrapper>
-				</ModalCongrats>
-			</ModalWrapper>
+			<ModalCongrats>
+				<ImgCat src="https://storage.googleapis.com/kidslikev2_bucket/4c67b0f578c6e7ff1d0b504f8019ffc9.png" />
+				<Congrats>Поздравляем! Ты получаешь:</Congrats>
+				<BtnClose type="button" autoFocus onClick={() => setOpen(false)}>
+					&#10005;
+				</BtnClose>
+				<PrizesWrapper>
+					{itemsAll.map(({ _id, title, imageUrl }) => (
+						<PrizesWrapper_block key={_id}>
+							<ChosenPrise img={imageUrl} />
+							<ChosenPrise_name>{title}</ChosenPrise_name>
+						</PrizesWrapper_block>
+					))}
+				</PrizesWrapper>
+			</ModalCongrats>
 		</ModalBackDrop>
 	);
 };
--- src/components/Commons/CongratsModal/CongratsModal.styles.js
+++ src/components/Commons/CongratsModal/CongratsModal.styles.js
@@ -83,7 +83,7 @@
 //     margin-top: 202px;
 //     width: 504px;
 //     height: 135px;
-// // @media (min-width: 768px) {  
+// // @media (min-width: 768px) {
 // //     flex-direction: row;
 // //     }
 // `
@@ -158,11 +158,11 @@
 //     margin-top: 10%;
 //     justify-content: center;
 //     width: 280px;
-//     height: 484px;  
+//     height: 484px;
 //     inset: 0px;
 //     border: none;
 //     border-radius: 6px;
-// @media (min-width: 768px) {  
+// @media (min-width: 768px) {
 //     width: 602px;
 //     height: 390px;
 // }
@@ -206,24 +206,43 @@ export const Congrats = styled.h2`
 	font-family: Montserrat;
 	font-style: normal;
 	font-weight: 600;
-    font-size: 18px;
-    line-height: 22px;
+	font-size: 18px;
+	line-height: 22px;
 	text-align: center;
 	@media ${device.tablet} {
 		width: 504px;
 		height: 36px;
 		font-size: 30px;
-	    line-height: 37px;
+		line-height: 37px;
 	}
 `;
 
-export const BtnClose = styled.img`
+export const BtnClose = styled.button`
 	position: absolute;
 	right: 10px;
 	top: 10px;
-	width: 14px;
-	height: 14px;
+	width: 18px;
+	height: 18px;
 	cursor: pointer;
+	border: none;
+	font-size: 22px;
+	line-height: 22px;
+	font-weight: 300;
+	background-color: transparent;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding-top: 2px;
+	transition: all 250ms linear;
+
+	&:focus {
+		outline: 0.3px solid #a4b0be;
+	}
+
+	&:hover {
+		color: #000;
+	}
+
 	@media ${device.tablet} {
 		right: 20px;
 		top: 20px;
@@ -248,28 +267,32 @@ export const CongratsModalWrapper = styled.div`
 	}
 `;
 
-export const PrizesWrapper =styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    margin-top: 202px;
-    width: 504px;
-    height: 135px;
-`
+export const PrizesWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	flex-wrap: wrap;
+	margin-top: 202px;
+	width: 504px;
+	height: 135px;
+`;
 
 export const PrizesWrapper_block = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 100px;
-	height: 135px;
+	align-items: center;
 `;
 
-export const ChosenPrise = styled.img`
+export const ChosenPrise = styled.div`
 	width: 90px;
 	height: 90px;
 	border: 5px solid #ffbc33;
 	border-radius: 50%;
+	background-image: url(${({ img }) => img});
+	background-position: center center;
+	background-size: cover;
+	background-repeat: no-repeat;
+	margin-bottom: 10px;
 `;
 
 export const ChosenPrise_name = styled.h2`
@@ -287,11 +310,10 @@ export const ChosenPrise_name = styled.h2`
 `;
 
 export const ModalWrapper = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `;
 
 export const WrapperApasity = styled.div`
@@ -302,38 +324,37 @@ export const WrapperApasity = styled.div`
 `;
 
 export const AwardsSubmitData = styled.button`
-    display:flex;
-    justify-content: center;
-    margin: 40px auto;
-    width: 220px;
-    height: 48px;
-    background: #FFBC33;
-    border-radius: 6px;
-    border: none;
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 15px;
-    align-items: center;
-    letter-spacing: 0.02em;
-    color: #FAFAFA;
+	display: flex;
+	justify-content: center;
+	margin: 40px auto;
+	width: 220px;
+	height: 48px;
+	background: #ffbc33;
+	border-radius: 6px;
+	border: none;
+	font-family: Montserrat;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 12px;
+	line-height: 15px;
+	align-items: center;
+	letter-spacing: 0.02em;
+	color: #fafafa;
 `;
 
-export const  ModalCongrats = styled.div`
-    position: relative;
-    background: white;
-    display: flex;
-    margin: auto;
-    margin-top: 10%;
-    justify-content: center;
-    width: 280px;
-    height: 484px;  
-    inset: 0px;
-    border: none;
-    border-radius: 6px;
-@media (min-width: 768px) {  
-    width: 602px;
-    height: 390px;
-}
-`
+export const ModalCongrats = styled.div`
+	position: relative;
+	background: white;
+	display: flex;
+	margin: auto;
+	justify-content: center;
+	width: 280px;
+	height: 484px;
+	inset: 0px;
+	border: none;
+	border-radius: 6px;
+	@media (min-width: 768px) {
+		width: 602px;
+		height: 390px;
+	}
+`;
